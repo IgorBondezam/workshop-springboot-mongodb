82b849a1ffcb5f5209b3c8d222740de689cb4871
Igor Bondezam França <igorbfraca@hotmail.com>
2023-01-27 19:03:49 -0300

    User by id

--- src/main/java/com/igorbondezam/workshopmongo/resources/UserResource.java
+++ src/main/java/com/igorbondezam/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.igorbondezam.workshopmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,15 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable  String id) {
+
+        User obj = service.findById(id);
+
+        return ResponseEntity.ok().body(new UserDTO(obj));
 
     }
 
---
+++ src/main/java/com/igorbondezam/workshopmongo/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.igorbondezam.workshopmongo.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
