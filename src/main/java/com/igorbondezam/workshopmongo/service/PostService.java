b35b0a8bbf83aba8a2534d36b8352bf5ebe5e99e
Igor Bondezam França <igorbfraca@hotmail.com>
2023-01-27 23:18:28 -0300

    fullSearch

--- src/main/java/com/igorbondezam/workshopmongo/service/PostService.java
+++ src/main/java/com/igorbondezam/workshopmongo/service/PostService.java
@@ -9,6 +9,7 @@ import com.igorbondezam.workshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +28,11 @@ public class PostService {
 
     public List<Post> findyByTitle(String text){
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime()+24*60*60*1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 
 }
